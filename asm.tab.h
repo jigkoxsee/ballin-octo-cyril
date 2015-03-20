d86151d4f39d0c577d77942be47ff1470fd68803
jigkoxsee <ked.naja@gmail.com>
2015-03-20 19:43:39 +0700

    add Makefile

---
+++ asm.tab.h
@@ -0,0 +1,91 @@
+/* A Bison parser, made by GNU Bison 3.0.2.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_ASM_TAB_H_INCLUDED
+# define YY_YY_ASM_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    ACC = 258,
+    TOP = 259,
+    SIZE = 260,
+    REGISTER = 261,
+    SHOW = 262,
+    COPY = 263,
+    TO = 264,
+    PUSH = 265,
+    POP = 266,
+    NUMBER = 267,
+    PLUS = 268,
+    MINUS = 269,
+    TIMES = 270,
+    DIVIDE = 271,
+    MOD = 272,
+    POWER = 273,
+    LEFT = 274,
+    RIGHT = 275,
+    ALEFT = 276,
+    ARIGHT = 277,
+    BLEFT = 278,
+    BRIGHT = 279,
+    END = 280,
+    OTHERS = 281,
+    AND = 282,
+    OR = 283,
+    NOT = 284,
+    NEG = 285
+  };
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_ASM_TAB_H_INCLUDED  */
